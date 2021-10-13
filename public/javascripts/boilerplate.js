5df4aee8ff8c527f2245913383c0aec8e55ec52e
Zach Sirotkin <zsirotkin@hotmail.com>
2021-10-12 19:10:53 -0600

    Added contact and home links and refactored footer

--- public/javascripts/boilerplate.js
+++
@@ -1,7 +0,0 @@
-const john = document.querySelector('.john')
-const scrollDown = document.querySelector('.scrollDown')
-
-
-scrollDown.addEventListener('click', function () {
-    john.scrollIntoView(true)
-})
