a48f53af382fdfbf532a8a1b0d21906bec63cd53
Zach Sirotkin <zsirotkin@hotmail.com>
2021-10-22 15:24:13 -0600

    contact form nodemailer configured + .env file

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const nodemailer = require('nodemailer');
+
+if (process.env.NODE_ENV !== 'production') { //if we ar enot in production mode
+    require('dotenv').config();//require our .env file,
+}
+
+const email = process.env.EMAIL
 
 app.set('views', path.join(__dirname, 'views'));
 
@@ -16,11 +23,39 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 app.get('/contact', (req, res) => {
     res.render('contact')
+})
+app.post('/contact', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.MAILPASS,
+        }
+    })
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.EMAIL,
+        subject: `Message from ${req.body.name}: ${req.body.subject}`,
+        text: `Phone Number: ${req.body.phone}
+        Email: ${req.body.email} 
+        Message: ${req.body.message}`
+
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error')
+        } else {
+            console.log('Email Sent: ' + info.response)
+            res.send('success')
+        }
+    })
 })
 app.get('/about', (req, res) => {
     res.render('about')
@@ -28,6 +63,9 @@ app.get('/about', (req, res) => {
 app.get('/services', (req, res) => {
     res.render('services')
 })
+
+
+
 
 
 
